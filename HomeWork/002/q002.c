2922e346e2dff4ffc2833322995ec9088af9cd10
WalkingMen666 <breezekaka66@gmail.com>
2024-02-27 10:01:32 +0800

    Someone ask me to change the variable name of q002:virturalRoot => imaginary.

--- HomeWork/002/q002.c
+++ HomeWork/002/q002.c
@@ -2,22 +2,22 @@
 #include <math.h>
 
 int main(){
-    float a = 0, b = 0, c = 0, realRoot = 0, virtualRoot = 0;
+    float a = 0, b = 0, c = 0, realRoot = 0, imaginary = 0;
 
     scanf("%f %f %f", &a, &b, &c);
 
     realRoot = -b/(2*a);
-    virtualRoot = b*b-4*a*c;
+    imaginary = b*b-4*a*c;
 
-    if(virtualRoot < 0){
-        virtualRoot = sqrt(fabs(virtualRoot))/(2*a);
-        printf("%.1f+%.1fi\n", realRoot, virtualRoot);
-        printf("%.1f-%.1fi\n", realRoot, virtualRoot);
+    if(imaginary < 0){
+        imaginary = sqrt(fabs(imaginary))/(2*a);
+        printf("%.1f+%.1fi\n", realRoot, imaginary);
+        printf("%.1f-%.1fi\n", realRoot, imaginary);
     }
     else{
         if(realRoot == -0.0) realRoot = 0.0;
-        printf("%.1f\n", realRoot+(sqrt(virtualRoot)/(2*a)));
-        printf("%.1f\n", realRoot-(sqrt(virtualRoot)/(2*a)));
+        printf("%.1f\n", realRoot+(sqrt(imaginary)/(2*a)));
+        printf("%.1f\n", realRoot-(sqrt(imaginary)/(2*a)));
     }
 
     return 0;
--- Test/main.c
+++ Test/main.c
@@ -1,82 +1,23 @@
-//  ======================================================== 
-//  ==========      測試你的程式在那些測資出錯      ==========
-//  ======================================================== 
-
 #include <stdio.h>
-
-void scoreCal(int pad, int *score){
-    while(pad > 0){
-        *score += pad&1;
-        pad >>= 1;
+char encode(int a, int b, int c, int d){
+    switch(a*1000+b*100+c*10+d) {
+        case 101:
+            return 'A';
+        case 111:
+            return 'B';
+        case 10:
+            return 'C';
+        case 1101:
+            return 'D';
+        case 1000:
+            return 'E';
+        case 1100:
+            return 'F';
     }
 }
-
-int mySol(int player[5]){
-    int score = 0, out = 0, pad = 0, tempPad = 0;
-
-    for(int i = 0; i < 5; i++){
-        player[i] != 0 ? pad = (pad << player[i]) | (1 << (player[i] - 1)) : out++;
-        if(out >= 3){
-            scoreCal(pad>>3, &score);
-            out = 0;
-            pad = 0;
-        }
-    }
-    scoreCal(pad>>3, &score);
-    
-    return score;
-}
-
-int yourSol(int player[5]){
-    // 把下面的程式碼換成自己的然後回傳分數
-    // 打者的打擊情況要用player[5]操作
-    int score = 0, out = 0;
-    int pad[3] = {0};
-
-    for(int i = 0; i < 5; i++){
-        if(player[i] > 0){
-            if(pad[2] == 1) score++;
-            pad[2] = pad[1];
-            pad[1] = pad[0];
-            pad[0] = 1;
-            for(int j = 0; j < player[i]-1; j++){
-                if(pad[2] == 1) score++;
-                pad[2] = pad[1];
-                pad[1] = pad[0];
-                pad[0] = 0;
-            }
-        }
-        else out++;
-
-        if(out == 3){
-            for(int j = 0; j < 3; j++) pad[j] = 0;
-            out = 0;
-        }
-    }
-
-    return score;
-}
-
 int main(){
-    int error = 0;
-    for(int a = 0; a < 5; a++){
-        for(int b = 0; b < 5; b++){
-            for(int c = 0; c < 5; c++){
-                for(int d = 0; d < 5; d++){
-                    for(int e = 0; e < 5; e++){
-                        int player[5] = {a, b, c, d, e};
-                        int myAns = mySol(player);
-                        int yourAns = yourSol(player);
-                        if(myAns != yourAns){
-                            for(int i = 0; i < 5; i++) printf("%d", player[i]);
-                            printf("\nbit: %d\ngen: %d\n==========\n", myAns, yourAns);
-                            error++;
-                        }
-                    }
-                }
-            }    
-        }
-    }
-    printf("You got %d errors!", error);
+    int a=0, b=0, c=0, d=0;
+    scanf("%d %d %d %d",&a, &b, &c, &d);
+    printf("%c\n", encode(a,b,c,d));
     return 0;
 }
